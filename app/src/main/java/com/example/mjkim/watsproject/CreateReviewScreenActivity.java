52886174120c9f54d795ebb2bf62ef0a5093c7ee
mjkim <eoqkrskfk94@naver.com>
2019-01-22 15:52:27 +0900

    1월 22일 1. 리뷰 등록 (이름 + 주소 형식)

--- app/src/main/java/com/example/mjkim/watsproject/CreateReviewScreenActivity.java
+++ app/src/main/java/com/example/mjkim/watsproject/CreateReviewScreenActivity.java
@@ -171,8 +171,11 @@ public class CreateReviewScreenActivity extends AppCompatActivity {
 
         reviewList = new ReviewList(location_name, location_addess, location_number, location_category, review_description, location_mapx, location_mapy, tag1, tag2, tag3, tag4, tag5, tag6, mTime, userName);
 
-        String nameAndAdress = ""; //이름이랑 주소 같이 나오는 스트링
-        reviewData.saveData(location_name, reviewList);
+        String nameAndAdress = ""; //이름이랑 주소 같이 나오는 스트
+
+        nameAndAdress = location_name + " , " + location_addess;
+
+        reviewData.saveData(nameAndAdress, reviewList);
 
 
         myDialog.setContentView(R.layout.review_popup);

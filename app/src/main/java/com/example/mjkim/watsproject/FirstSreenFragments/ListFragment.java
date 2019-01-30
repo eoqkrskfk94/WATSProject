40ee797e827e8d84e2bb0cb7b7b0286488f4fab5
mjkim <eoqkrskfk94@naver.com>
2019-01-30 16:42:08 +0900

    1월 30일 1. 장소 리스트 띄우기 성공

--- app/src/main/java/com/example/mjkim/watsproject/FirstSreenFragments/ListFragment.java
+++ app/src/main/java/com/example/mjkim/watsproject/FirstSreenFragments/ListFragment.java
@@ -1,18 +1,65 @@
 package com.example.mjkim.watsproject.FirstSreenFragments;
 
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.mjkim.watsproject.MainScreenActivity;
+import com.example.mjkim.watsproject.MyReviewScreenActivity;
 import com.example.mjkim.watsproject.R;
+import com.example.mjkim.watsproject.Review.CategoryAdapter;
+import com.example.mjkim.watsproject.Review.ReviewAdapter;
+import com.example.mjkim.watsproject.Review.ReviewList;
+import com.example.mjkim.watsproject.Review.UserReviewAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ListFragment extends Fragment {
+
+    private CategoryAdapter reviewAdapter;
+    Dialog myDialog;
+    // 리뷰 정보 담을 변수
+    private ReviewList reviewList;
+    public static ArrayList<ReviewList> reviewLists;
+    FirebaseAuth auth;
+    private DatabaseReference mDatabase;
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private String locationName, key, reviewerName, reviewDate, reviewDescription, locationNumber, userEmail, userName, locationCategory, locationAddress;
+    private Boolean tag1, tag2, tag3, tag4, tag5, tag6;
+    private String imageUrl1, imageUrl2, imageUrl3, imageUrl4, imageUrl5, imageUrl6, imageUrl7, imageUrl8, imageUrl9;
+    private double mapx, mapy;
+    static public int totalLocationCount;
+
+    final Context context;
+
+    {
+        context = getContext();
+    }
+
 
 
     public ListFragment() {
@@ -23,8 +70,145 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View v = inflater.inflate(R.layout.fragment_list, container,false);
+
+
+        reviewLists = new ArrayList<ReviewList>();
+        totalLocationCount = 0;
+
+        mDatabase = database.getReference();
+        final DatabaseReference mymyreview = database.getReference();
+
+
+        mDatabase.child("review lists").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                System.out.println("키값: " + dataSnapshot.getKey());
+
+
+                mymyreview.child("review lists").child(dataSnapshot.getKey()).addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        ReviewList myreview = dataSnapshot.getValue(ReviewList.class);
+                        key=dataSnapshot.getKey();
+                        locationName = myreview.getLocation_name();
+                        System.out.println("모든 이름: "+locationName);
+                        reviewDate = myreview.getDate();
+                        tag1 = myreview.getTag1();
+                        tag2 = myreview.getTag2();
+                        tag3 = myreview.getTag3();
+                        tag4 = myreview.getTag4();
+                        tag5 = myreview.getTag5();
+                        tag6 = myreview.getTag6();
+                        reviewDescription = myreview.getReview_description();
+                        locationNumber = myreview.getPhone_number();
+                        locationCategory = myreview.getLocation_category();
+                        locationAddress = myreview.getLocation_address();
+                        mapx = myreview.getMapx();
+                        mapy = myreview.getMapy();
+                        userEmail = myreview.getUserEmail();
+                        imageUrl1 = myreview.getImageUrl1();
+                        imageUrl2 = myreview.getImageUrl2();
+                        imageUrl3 = myreview.getImageUrl3();
+                        imageUrl4 = myreview.getImageUrl4();
+                        imageUrl5 = myreview.getImageUrl5();
+                        imageUrl6 = myreview.getImageUrl6();
+                        imageUrl7 = myreview.getImageUrl7();
+                        imageUrl8 = myreview.getImageUrl8();
+                        imageUrl9 = myreview.getImageUrl9();
+
+                        reviewList = new ReviewList(locationName, locationAddress, locationNumber, locationCategory, reviewDescription, mapx, mapy,
+                                tag1, tag2, tag3, tag4, tag5, tag6, reviewDate, userName, key, imageUrl1, imageUrl2, imageUrl3, imageUrl4, imageUrl5, imageUrl6, imageUrl7, imageUrl8, imageUrl9);
+
+                        reviewList.setUserName(myreview.getUserName());
+
+                        reviewLists.add(totalLocationCount, reviewList);
+
+                        totalLocationCount++;
+
+                        // 리뷰 갯수
+                        //TextView review_count = (TextView) findViewById(R.id.review_count);
+                        //review_count.setText(Integer.toString(totalLocationCount));
+
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//                        ReviewList myreview = dataSnapshot.getValue(ReviewList.class);
+//                        System.out.println(dataSnapshot.getKey() + " was " + myreview.getEmail() +myreview.getDate()+ " meters tall.");
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+//                        ReviewList myreview = dataSnapshot.getValue(ReviewList.class);
+//                        System.out.println(dataSnapshot.getKey() + " was " + myreview.getEmail() +myreview.getDate()+ " meters tall.");
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//                        ReviewList myreview = dataSnapshot.getValue(ReviewList.class);
+//                        System.out.println(dataSnapshot.getKey() + " was " + myreview.getEmail() +myreview.getDate()+ " meters tall.");
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
+
+        myDialog = new Dialog(getContext()); //로딩 팝업 변수 선언
+        myDialog.setContentView(R.layout.loading_popup);
+        myDialog.setCancelable(false);
+
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
+
+        myDialog.show(); //회원가입 팝업창.
+
+        final Timer timer2 = new Timer();
+        timer2.schedule(new TimerTask() {
+            public void run() {
+                myDialog.dismiss();
+                timer2.cancel(); //this will cancel the timer of the system
+            }
+        }, 2400); // the timer will count 2.4 seconds....
+
+
+
+        // 리뷰 어댑터 끼우기
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ListView reviewListView = (ListView) v.findViewById(R.id.category_list);
+                reviewAdapter = new CategoryAdapter(getActivity(), reviewLists,context,getActivity(),totalLocationCount);
+                reviewListView.setAdapter(reviewAdapter);
+            }
+        }, 2300);
+
+
+        return v;
     }
 
 }

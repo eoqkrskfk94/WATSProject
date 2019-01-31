30fb2e03aeaa09b882b8fa630649f20e8b313b85
Nam <jjune095@naver.com>
2019-01-31 16:40:16 +0900

    1.31 내 리뷰 이미지 불러오기 수정 가능

--- app/src/main/java/com/example/mjkim/watsproject/ReviseReviewScreenActivity.java
+++ app/src/main/java/com/example/mjkim/watsproject/ReviseReviewScreenActivity.java
@@ -293,7 +293,6 @@ public class ReviseReviewScreenActivity extends AppCompatActivity {
 
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(ReviseReviewScreenActivity.this,
-            if (ActivityCompat.shouldShowRequestPermissionRationale(ReviseReviewScreen.this,
                     Manifest.permission.READ_CONTACTS)) {
 
             } else {
@@ -307,8 +306,7 @@ public class ReviseReviewScreenActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(ReviseReviewScreenActivity.this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(ReviseReviewScreen.this,
+            
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(ReviseReviewScreenActivity.this,
                     Manifest.permission.READ_EXTERNAL_STORAGE)) {
